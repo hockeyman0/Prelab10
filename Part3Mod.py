04b9945890124d03c12dee53e711e900386e1ac4
Christopher Holly <hockeyman0@gmail.com>
2013-03-08 18:37:40 -0600

    First Commit

---
+++ Part3Mod.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python2.6
+
+#$Author$
+#$Date$
+#$Revision$
+#$HeadURL$
+
+import sys
+import re
+import os
+
---
+++ problem7.py
@@ -0,0 +1,36 @@
+import sys
+import re
+import os
+import math
+
+
+class Point2D:
+	def __init__(self, x=0.0, y=0.0):
+		self.x = float(x)
+		self.y = float(y)
+	
+	def dist_between(self, other):
+		# Compute the distance between this point and another point
+		xsq = (self.x - other.x) * (self.x - other.x)
+		ysq = (self.y - other.y) * (self.y - other.y)
+		return math.sqrt(xsq + ysq)
+		
+		
+points = range(10)
+PointList = []
+for I in points:
+	for J in points:
+		tmp = Point2D(0,0)
+		tmp.x = (I+1)
+		tmp.y = (J+1)
+		PointList.append(tmp)
+		#print"(%.1f, %.1f)" % (tmp.x, tmp.y)
+		
+#print PointList[0].x
+
+#for I in points:
+#	for J in points:
+#		temp1 = PointList[]
+		
+for I in PointList:
+	print I.dist_between(PointList[0])
