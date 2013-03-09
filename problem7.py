f50130c34ae34a96de87d1e72cda4546eb15ac41
Christopher Holly <hockeyman0@gmail.com>
2013-03-09 16:27:46 -0600

    16:27 3/9/13

--- problem7.py
+++ problem7.py
@@ -5,6 +5,9 @@ import math
 
 
 def load_points(datafile):
+	if (os.access(datafile, os.R_OK) == 0):
+		sys.exit(1)
+	InFile = open(datafile, "r")
 	
 
 
@@ -20,6 +23,24 @@ class Point2D:
 		return math.sqrt(xsq + ysq)
 		
 		
+class MyPoint2D(Point2D):
+	def __init__(self, x, y):
+		Point2D.__init__(self,0.0,0.0)
+		self.x = float(x)
+		self.y = float(y)
+		
+	def get_max_coord(self):
+		if self.x > self.y:
+			return self.x
+		else:
+			return self.y
+	def get_min_coord(self):
+		if self.x < self.y:
+			return self.x
+		else:
+			return self.y
+		
+		
 points = range(10)
 PointList = []
 for I in points:
@@ -28,13 +49,12 @@ for I in points:
 		tmp.x = (I+1)
 		tmp.y = (J+1)
 		PointList.append(tmp)
-		#print"(%.1f, %.1f)" % (tmp.x, tmp.y)
-		
-#print PointList[0].x
 
-#for I in points:
-#	for J in points:
-#		temp1 = PointList[]
 		
 for I in PointList:
 	print I.dist_between(PointList[0])
+	
+	
+testpoint = MyPoint2D(5,9)
+print testpoint.get_max_coord()
+print testpoint.get_min_coord()
